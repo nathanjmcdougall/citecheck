fc29608d6734f9d2c7e3c507245277668e344117
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2023-04-10 19:54:16 +1200

    Remove incorrect Generic typing

--- src/citecheck/core/citeas.py
+++ src/citecheck/core/citeas.py
@@ -1,12 +1,9 @@
 """Checking a citation of a Citable object, or turning it into one"""
 from dataclasses import dataclass
-from typing import Generic, TypeVar
 
 from citecheck.core.cited import Cited
 from citecheck.core.types.citable import Citable
 from citecheck.core.types.citation import Citation
-
-T = TypeVar("T", bound=Citable)
 
 
 @dataclass
@@ -15,12 +12,12 @@ class _BaseCiteAs:
     _citation: Citation
 
 
-class CiteAs(Generic[T]):
+class CiteAs:
     """Checking a citation of a Citable object, or turning it into one"""
 
     def __class_getitem__(cls, item: tuple[type[Citable], Citation]):
         value_cls, citation = item
         return _BaseCiteAs(_value_cls=value_cls, _citation=citation)
 
-    def __new__(cls, value: T, citation: Citation):
+    def __new__(cls, value: Citable, citation: Citation):
         return Cited[type(value)](value, _citation=citation)
