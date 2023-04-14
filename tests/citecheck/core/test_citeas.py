0453e43c4f01f2066ebe4148c2a612b935155934
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2023-04-14 16:22:42 +1200

    Combine tests into a single file

--- tests/citecheck/core/test_citeas.py
+++ tests/citecheck/core/test_citeas.py
@@ -52,3 +52,19 @@ class TestCiteAs:
         base_type = type(value)
 
         assert isinstance(CiteAs(value, "example"), base_type)
+
+    def test_basic(self):
+        value = 5
+        citable = CiteAs(value, "math")
+        assert citable == value
+        # pylint: disable=protected-access
+        assert citable._citation == "math"
+        # pylint: enable=protected-access
+        assert repr(citable) == f"{value} (cited as math)"
+        assert hash(citable) == hash((value, "math"))
+        assert citable + 2 == 7
+
+    def test_equality(self):
+        value = 5
+        citation = "math"
+        assert CiteAs(value, citation) == CiteAs(value, citation)
